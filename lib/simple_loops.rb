9b93c40c91e78aab477a07dcbc6b113e67cc3138
Nate Croteau <natekcroteau@gmail.com>
2020-11-16 22:53:43 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,10 +1,10 @@
 # Write your methods here
 
-def loop_message_five_times
+def loop_message_five_times(string)
   counter = 0 
   
   while counter <5 do
-    puts "string"
+    puts string
     counter += 1 
   end
 end
