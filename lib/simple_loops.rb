bafb12e808e992c47169674a21e3c14f988c0fd4
Nate Croteau <natekcroteau@gmail.com>
2020-11-16 22:53:01 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,22 @@
 # Write your methods here
+
+def loop_message_five_times
+  counter = 0 
+  
+  while counter <5 do
+    puts "string"
+    counter += 1 
+  end
+end
+
+def loop_message_n_times
+  
+end
+
+def output_array
+  
+end
+
+def return_string_array
+  
+end
