e297cde811fb332ccf8be1f2d6a38e0b0844d398
Nate Croteau <natekcroteau@gmail.com>
2020-11-16 23:20:53 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -27,11 +27,10 @@ end
 
 def return_string_array(array)
   counter = 0 
-  second_array = []
-  
+  return_array = []
   while counter < array.length do
-    puts array[counter]
-      p second_array.push
+    return_array.push(array[counter].to_s)
     counter +=1 
   end    
+  return_array
 end
